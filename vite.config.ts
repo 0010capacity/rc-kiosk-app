b3105e986ce17ebaeed5f23c6ee6702c1c819204
이정원 <won31080@gmail.com>
2025-03-22 20:50:20 +0900

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/rc-kiosk-app/',
+  base: '/gift-kiosk-app/',
   plugins: [react()],
 });
