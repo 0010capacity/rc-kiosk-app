bca48a09112d688357adaa55d7a3a0832a664ef6
이정원 <won31080@gmail.com>
2025-03-22 22:28:18 +0900

    apply routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProductSelector from "./pages/ProductSelector";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
-  return <ProductSelector />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductSelector />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
