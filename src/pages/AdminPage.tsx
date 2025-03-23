67b06d5faf4f07b426388bf3418b49bbb81f32f1
이정원 <won31080@gmail.com>
2025-03-23 13:12:57 +0900

    adopt hash router

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -58,6 +58,7 @@ export default function AdminPage() {
                 <Trash2 size={16} />
               </button>
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
               <p className="font-semibold">
                 {record.name}{" "}
                 <span className="text-sm text-gray-400">
@@ -73,6 +74,9 @@ export default function AdminPage() {
                   )
                 </span>
               </p>
+=======
+              <p className="font-semibold">{record.name} <span className="text-sm text-gray-400">({record.timestamp ? new Date(record.timestamp.seconds * 1000).toLocaleString("ko-KR", { year: "2-digit", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" }) : ""})</span></p>
+>>>>>>> Stashed changes
 =======
               <p className="font-semibold">{record.name} <span className="text-sm text-gray-400">({record.timestamp ? new Date(record.timestamp.seconds * 1000).toLocaleString("ko-KR", { year: "2-digit", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" }) : ""})</span></p>
 >>>>>>> Stashed changes
