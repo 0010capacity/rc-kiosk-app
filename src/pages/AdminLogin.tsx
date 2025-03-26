4243c518777b370ebb3861ce4a1828ff230f302a
이정원 <won31080@gmail.com>
2025-03-26 22:41:14 +0900

    Update AdminLogin.tsx

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { supabase } from "@/lib/supabaseConfig";
+import { supabase } from "@supabase/supabase-js";
 
 export default function AdminLogin({ onBack }: { onBack: () => void }) {
   const [username, setUsername] = useState("");
