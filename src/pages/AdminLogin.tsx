27b9a425d5e6d44897f54b780d83fed3bda8a9f6
이정원 <won31080@gmail.com>
2025-03-26 23:06:56 +0900

    Update AdminLogin.tsx

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -48,9 +48,6 @@ export default function AdminLogin({ onBack }: { onBack: () => void }) {
           onChange={(e) => setPassword(e.target.value)}
         />
         <div className="flex justify-between">
-          <Button onClick={() => onBack()} variant="outline">
-            돌아가기
-          </Button>
           <Button onClick={handleLogin}>
             로그인
           </Button>
