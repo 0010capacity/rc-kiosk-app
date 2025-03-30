9f3acd8b46f51b9e4df9777dde0c61a3babecac3
이정원 <won31080@gmail.com>
2025-03-30 11:46:09 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import './index.css';
+// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ProductSelector from './pages/ProductSelector';
-import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <ProductSelector />
+      <Routes>
+        <Route path="/location/:locationId" element={<ProductSelector />} />
+      </Routes>
     </HashRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
