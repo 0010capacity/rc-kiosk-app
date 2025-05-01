e4d27f7f67b03cbdd1b6c18da1655e541c217e88
이정원 <won31080@gmail.com>
2025-05-01 11:17:37 +0900

    relative size

--- src/pages/ProductSelector.tsx
+++ src/pages/ProductSelector.tsx
@@ -186,7 +186,7 @@ export default function ProductSelector() {
       onMouseLeave={() => setShowTooltipId(null)}
     >
       <div
-        className="relative w-48 h-24"
+        className="relative w-full aspect-[2/1]"
         onTouchStart={() => setShowTooltipId(item.id)}
         onMouseEnter={() => setShowTooltipId(item.id)}
       >
